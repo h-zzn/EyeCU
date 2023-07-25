e5f1519bbcb0d214cd282e4aca8d64d380b7765f
dudwh2275 <22000128@handong.ac.kr>
2023-07-25 18:26:16 +0900

    Swords Collision & Cube Destroy 할 때 Haptic Feedback

--- Assets/Scripts/Sword/BlueSwordCubeCollision.cs
+++ Assets/Scripts/Sword/BlueSwordCubeCollision.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static OVRHaptics;
 
 public class BlueSwordCubeCollision : MonoBehaviour
 {
     public OVRInput.Controller controllerType; // 컨트롤러 종류 선택
     public float vibrationDuration = 1.0f; // 햅틱 지속 시간
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("blueCube"))
         {
             // 충돌한 오브젝트가 blueCube 태그를 가지고 있다면 해당 오브젝트를 삭제합니다.
             Destroy(collision.gameObject);
+            Debug.Log("aaa");
 
             // Oculus 컨트롤러에서 햅틱 반응을 발생시킵니다.
-            OVRInput.SetControllerVibration(1.0f, vibrationDuration, controllerType);
+            OVRHapticsClip clip = new OVRHapticsClip();
+            for (int i = 0; i < Config.SampleRateHz * vibrationDuration; i++)
+            {
+                byte sample = (byte)(1.0f * 255);
+                clip.WriteSample(sample);
+            }
+
+            OVRHapticsChannel hapticsChannel = (controllerType == OVRInput.Controller.LTouch) ? LeftChannel : RightChannel;
+            hapticsChannel.Preempt(clip);
+
+            // 햅틱 반응 시간 이후에 반응을 중지시킵니다.
+            StartCoroutine(StopVibration());
         }
     }
-}
 
+    private IEnumerator StopVibration()
+    {
+        // 햅틱 반응 시간 이후에 반응을 중지시킵니다.
+        yield return new WaitForSeconds(vibrationDuration);
+
+        // Oculus 컨트롤러의 햅틱 반응을 중지합니다.
+        OVRHapticsChannel hapticsChannel = (controllerType == OVRInput.Controller.LTouch) ? LeftChannel : RightChannel;
+        hapticsChannel.Clear();
+    }
+}
