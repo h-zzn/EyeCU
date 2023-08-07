4c4a09a364c24aec724789646152afaf6887f997
dudwh2275 <22000128@handong.ac.kr>
2023-08-07 15:08:17 +0900

    StartScene - VR Controller movement 구현

---
+++ Assets/Scripts/Managers/VRControllerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class VRControllerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Transform playerTransform;
+    public float moveSpeed = 1.5f; // Adjust this value to control the movement speed.
+    private float rotationSpeed = 2.0f;
+    private OVRCameraRig ovrCameraRig;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerTransform = transform;
+        ovrCameraRig = FindObjectOfType<OVRCameraRig>(); // Find the OVRCameraRig component in the scene.
+    }
+
+    void Update()
+    {
+        // Get the forward direction of the HMD (Head Mounted Display) from the OVRCameraRig.
+        Vector3 hmdForward = ovrCameraRig.centerEyeAnchor.forward;
+        hmdForward.y = 0f;
+        hmdForward.Normalize();
+
+        // Get the thumbstick input.
+        Vector2 thumbstickInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+        // Rotate the player based on thumbstick horizontal input.
+        playerTransform.Rotate(Vector3.up * thumbstickInput.x * rotationSpeed);
+
+        // Calculate the movement direction based on HMD forward and thumbstick vertical input.
+        Vector3 moveDirection = hmdForward * thumbstickInput.y;
+
+        // Make the player move in the direction of the HMD facing.
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+}
