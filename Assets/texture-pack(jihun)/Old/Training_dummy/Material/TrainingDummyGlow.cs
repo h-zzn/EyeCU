9e18eaea70c63f5f87ee3902b820f108d5057c4e
dudwh2275 <22000128@handong.ac.kr>
2023-09-03 20:56:56 +0900

    초기 세팅

---
+++ Assets/texture-pack(jihun)/Old/Training_dummy/Material/TrainingDummyGlow.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingDummyGlow : MonoBehaviour
+{
+    // Success Material
+    [SerializeField] private Material successMaterial;
+    // Normal Material
+    [SerializeField] private Material normalMaterial;
+
+    [SerializeField] private GameObject good;
+
+    // Get the skinned mesh renderer
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+
+    [SerializeField] private bool isGlowing = false;
+
+    [SerializeField] private float glowTime = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGlowing)
+        {
+            skinnedMeshRenderer.material = successMaterial;
+            good.SetActive(true);
+        }
+        else
+        {
+            skinnedMeshRenderer.material = normalMaterial;
+            good.SetActive(false);
+        }
+    }
+
+    // Set the glowing on and turn off after 1 second
+    public void SetGlowing()
+    {
+        isGlowing = true;
+        Invoke("SetNotGlowing", glowTime);
+        
+    }
+
+    // Set the glowing off
+    private void SetNotGlowing()
+    {
+        isGlowing = false;
+        
+    }
+}
