8d1c58e74d4779a1be39d90746d9838e7cf072e5
DESKTOP-MV8T918\cglab <yslee551@handong.ac.kr>
2023-09-10 18:40:33 +0900

    Hand Effect Haptic 추가

--- Assets/Scripts/Hand/HandEffectCollision.cs
+++ Assets/Scripts/Hand/HandEffectCollision.cs
@@ -1,12 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using static OVRHaptics;
 
 public class HandEffectCollision : MonoBehaviour
 {
+    public OVRInput.Controller controllerType; // 컨트롤러 종류 선택
+    public float vibrationDuration = 2.0f; // 햅틱 지속 시간
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("HandEffect"))
         {
             Debug.Log("Collision Detected with: " + other.name);
+
+            // Oculus 컨트롤러에서 햅틱 반응을 발생시킵니다.
+            OVRHapticsClip clip = new OVRHapticsClip();
+            for (int i = 0; i < Config.SampleRateHz * vibrationDuration; i++)
+            {
+                byte sample = (byte)(1.0f * 255);
+                clip.WriteSample(sample);
+            }
+
+            OVRHapticsChannel hapticsChannel = (controllerType == OVRInput.Controller.LTouch) ? LeftChannel : RightChannel;
+            hapticsChannel.Preempt(clip);
+
+            // 햅틱 반응 시간 이후에 반응을 중지시킵니다.
+            StartCoroutine(StopVibration());
 
             // Deactivate children of this GameObject
             DeactivateChildren(this.gameObject);
@@ -41,5 +61,15 @@ public class HandEffectCollision : MonoBehaviour
         {
             child.gameObject.SetActive(true);
         }
+    }
+
+    private IEnumerator StopVibration()
+    {
+        // 햅틱 반응 시간 이후에 반응을 중지시킵니다.
+        yield return new WaitForSeconds(vibrationDuration);
+
+        // Oculus 컨트롤러의 햅틱 반응을 중지합니다.
+        OVRHapticsChannel hapticsChannel = (controllerType == OVRInput.Controller.LTouch) ? LeftChannel : RightChannel;
+        hapticsChannel.Clear();
     }
 }
