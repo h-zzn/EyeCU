6db987aff8b94c903c4a38a28f103d74b63d945b
Hyeonjin20 <22000205@handong.ac.kr>
2023-09-13 01:52:30 +0900

    startScene 맨 처음 세계관 내용 추가

--- Assets/Scripts/Hyunjin/gameStartScript.cs
+++ Assets/Scripts/Hyunjin/gameStartScript.cs
@@ -14,25 +14,36 @@ public class gameStartScript : MonoBehaviour
     private MeshCollider knifeMeshCollider;
     private MeshCollider bookMeshCollider;
 
+    [SerializeField] private GameObject canvasObj;
+
+    //canvas Animator
+    Animator canvasAnimator;
+
+
     float timer = 0.0f;
 
 
     void Start(){
+        //PlayerPrefs.DeleteKey("knifeActive"); 
+        canvasAnimator = canvasObj.transform.gameObject.GetComponent<Animator>();
+        canvasAnimator.SetBool("isStory", true);
+
         knifeMeshCollider = GameObject.Find("knife").GetComponent<MeshCollider>();
         bookMeshCollider = GameObject.Find("book").GetComponent<MeshCollider>();
 
         print("나이프 키 = "  + PlayerPrefs.GetInt("knifeActive"));
 
-        if(PlayerPrefs.HasKey("knifeActive")){
+        if(PlayerPrefs.HasKey("knifeActive")){  // 튜토리얼을 완료했다면
             knifeMeshCollider.enabled = true;
             bookMeshCollider.enabled = true;
+            canvasAnimator.SetBool("isStory", false);
         }
     }
 
     void Update(){
         if(!PlayerPrefs.HasKey("knifeActive"))
             timer += Time.deltaTime;
-            if(timer > 10.0f){
+            if(timer > 5.0f){
                 bookMeshCollider.enabled = true;
                 //print("book");
             }
